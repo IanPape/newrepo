2c203460d6efa93ea40bae288f1037999059d800
Ian Pape <ianjamespape@gmail.com>
2023-09-29 09:17:53 -0400

    pushing destructuring exercise to github

--- destructuringex.js
+++ destructuringex.js
@@ -119,6 +119,41 @@ arr[1] = temp;
 
 /* Write an ES2015 Version */
 
+//  ********** 8.[arr[0], arr[1]] = [arr[1], arr[0]]
+
+// ## **raceResults()**
+
+// Write a function called ***raceResults*** which accepts a single array argument. It should return an object with the keys ***first***, ***second***, ***third***, and ***rest***.
+
+// - first: the first element in the array
+// - second: the second element in the array
+// - third: the third element in the array
+// - rest: all other elements in the array
+
+// **Write a *one line* function to make this work using**
+
+// - An arrow function
+// - Destructuring
+// - `Enhanced` object assignment (same key/value shortcut)
+
+//  ********** 9. function raceResults(arr) {
+//   const [first, second, third, ...rest] = arr;
+  
+//   return {
+//     first,
+//     second,
+//     third,
+//     rest
+//   };
+// }
+
+// const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest});
+
+
+
+
+
+
 
 
 
